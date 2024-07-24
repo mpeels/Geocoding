5ed32e3d73b3ec501d935b87bb5430577bbd7d6a
Michael Peels <michaelpeels@Michael-Peels.local>
2024-07-24 12:35:45 -0400

    Updates cucumber tests to all use mock mvc

--- Geocoder/src/test/java/com/example/geocoder/steps/CensusApiServiceSteps.java
+++ Geocoder/src/test/java/com/example/geocoder/steps/CensusApiServiceSteps.java
@@ -2,6 +2,8 @@ package com.example.geocoder.steps;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,29 +11,16 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.example.geocoder.CucumberContextConfig;
+import com.example.geocoder.requests.AddressRequest;
+import com.example.geocoder.responses.CensusApiResponse;
+import com.example.geocoder.responses.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.java.en.And;
-
-
-import com.example.geocoder.CucumberContextConfig;
-import com.example.geocoder.exceptions.InvalidAddressException;
-import com.example.geocoder.exceptions.MissingStreetException;
-import com.example.geocoder.exceptions.MissingZipAndCityStateException;
-import com.example.geocoder.requests.AddressRequest;
-import com.example.geocoder.responses.AddressMatchResponse;
-import com.example.geocoder.responses.CensusApiResponse;
-import com.example.geocoder.responses.CoordinatesResponse;
-import com.example.geocoder.responses.ResultResponse;
-import com.example.geocoder.services.CensusApiService;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-
-
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -40,19 +29,14 @@ public class CensusApiServiceSteps extends CucumberContextConfig {
     @Autowired
     private MockMvc mockMvc;
 
-    private MvcResult mvcResult;
-
     @Autowired
-    private CensusApiService censusApiService;
+    private ObjectMapper mapper;
 
+    private MvcResult mvcResult;
     private AddressRequest addressRequest;
-    private CensusApiResponse response;
-    private Exception exception;
 
-    
     @Given("the application is running")
     public void applicationIsRunning() throws Exception {
-        AddressRequest address = new AddressRequest("1600 Pennsylvania Ave", "Washington", "DC", "20500"); 
         mvcResult = mockMvc.perform(MockMvcRequestBuilders
                 .get("/api/address/health")
                 .contentType(MediaType.APPLICATION_JSON))
@@ -60,72 +44,40 @@ public class CensusApiServiceSteps extends CucumberContextConfig {
                 .andReturn();
     }
 
-    @When("I submit a POST request to {string} with body {string}")
-    public void submitAPostRequestWithBody(String url, String body) throws Exception { 
-        mvcResult = mockMvc.perform(MockMvcRequestBuilders
-            .post(url)
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(body))
-            .andExpect(status().isOk())
-            .andReturn();
-    }
-    
-    @Then("the response status should be {int}")
-    public void responseStatusShouldBe200(int status) throws Exception {
-        assertThat(mvcResult.getResponse().getStatus()).isEqualTo(status);
-    }
-
-    @And("the response should contain a valid address match")
-    public void responseContainsValidAddressMatch() throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        String responseBody = mvcResult.getResponse().getContentAsString();
-        CensusApiResponse mockApiResponse = objectMapper.readValue(responseBody, CensusApiResponse.class);
-        
-        CoordinatesResponse expectedCoordinatesResponse = new CoordinatesResponse(-77.03654395730787, 38.89869091865549);
-        AddressMatchResponse expectedMatchResponse = new AddressMatchResponse("1600 PENNSYLVANIA AVE NW, WASHINGTON, DC, 20500", expectedCoordinatesResponse);
-        ResultResponse expectedResult = new ResultResponse(expectedMatchResponse);
-        CensusApiResponse expectedResponse = new CensusApiResponse(expectedResult);
-
-        assertThat(mockApiResponse).isEqualToComparingFieldByFieldRecursively(expectedResponse);
-    }
- 
-
-    @Given("an address request with missing street, city {string}, state {string}, and zip {string}")
-    public void missingStreetAddressRequest(String city, String state, String zip){
-        addressRequest = new AddressRequest("", city, state, zip);
-    }
-
-    @Given("an address request with street {string}, missing city, missing state, and missing zip")
-    public void missingCityStateZipAddressRequest(String street){
-        addressRequest = new AddressRequest(street, "", "", "");
-    }
-
     @Given("an address request with street {string}, city {string}, state {string}, and zip {string}")
-    public void invalidAddressRequest(String street, String city, String state, String zip){
+    public void invalidAddressRequest(String street, String city, String state, String zip) {
         addressRequest = new AddressRequest(street, city, state, zip);
     }
 
     @When("the address is submitted to the Census API")
-    public void theAddressIsSubmitted(){
-        try{
-            response = censusApiService.submitAddress(addressRequest);
-        }catch(Exception e){
-            exception = e; 
-        }
+    public void theAddressIsSubmitted() throws Exception {
+        mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/address/submittedAddress/request")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(addressRequest)))
+                .andReturn();
     }
 
-    @Then("a MissingStreetException should be thrown")
-    public void throwMissingStreetException(){
-        assertEquals(MissingStreetException.class, exception.getClass());
+    @Then("the response status should be {int}")
+    public void responseStatusShouldBe(int expected) {
+        assertEquals(expected, mvcResult.getResponse().getStatus());
     }
 
-    @Then("a MissingZipAndCityStateException should be thrown")
-    public void throwMissingZipAndCityStateException(){
-        assertEquals(MissingZipAndCityStateException.class, exception.getClass());
+    @Then("the response should contain a latitude of {double} and longitude of {double}")
+    public void responseContainsValidAddressMatch(Double latitude, Double longitude) throws Exception {
+        CensusApiResponse response = mapper.readValue(
+                mvcResult.getResponse().getContentAsString(),
+                CensusApiResponse.class);
+        assertThat(response.result().addressMatches().get(0).coordinates().x()).isEqualTo(latitude);
+        assertThat(response.result().addressMatches().get(0).coordinates().y()).isEqualTo(longitude);
     }
 
-    @Then("an InvalidAddressException should be thrown")
-    public void throwInvalidAddressException(){
-        assertEquals(InvalidAddressException.class, exception.getClass());
+    @Then("the response content should be an error")
+    public void responseIserror() throws Exception {
+        ErrorResponse response = mapper.readValue(
+                mvcResult.getResponse().getContentAsString(),
+                ErrorResponse.class);
+        assertThat(response.error()).isNotEmpty();
     }
+
 }
